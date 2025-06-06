a5d4343f9809c043412b247058cd464c5b9f2509
vaibhav <codweird5@gmail.com>
2025-06-06 09:21:06 +0530

    Added a buffer pdf download

--- routes/enquiryRoutes.js
+++ routes/enquiryRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createEnquiry } = require('../controllers/enquiryController');
+const { createEnquiry, downloadEnquiryPdf } = require('../controllers/enquiryController');
 
 router.post('/enquiry', createEnquiry);
+
+router.get('/enquiry/pdf/:id', downloadEnquiryPdf);
 
 module.exports = router;
