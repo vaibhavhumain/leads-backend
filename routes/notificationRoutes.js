52c7bc571c5df94c18d3a7ebcee47d5ee10e9efb
vaibhav <codweird5@gmail.com>
2025-06-10 14:38:21 +0530

    SFG

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { getNotifications, markRead } = require('../controllers/notificationController');
 const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
 
-
-router.get('/', getNotifications); 
-router.post('/mark-read/:id', markRead); 
+router.get('/', protect,getNotifications); 
+router.post('/mark-read/:id',protect, markRead); 
 
 module.exports = router;
