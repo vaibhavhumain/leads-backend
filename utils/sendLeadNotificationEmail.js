6521b57fc26a672c060c24ed44eed6c02bd2f7b3
vaibhav <codweird5@gmail.com>
2025-06-12 11:53:46 +0530

    SDFG

--- utils/sendLeadNotificationEmail.js
+++ utils/sendLeadNotificationEmail.js
@@ -1,6 +1,11 @@
 const transporter = require('../config/mailer');
 
-async function sendLeadNotificationEmail({to, leadId, leadDetails, forwardedBy}) {
+async function sendLeadNotificationEmail({ to, leadId, leadDetails, forwardedBy }) {
+  let contactNumbers = 'N/A';
+  if (Array.isArray(leadDetails.contacts) && leadDetails.contacts.length > 0) {
+    contactNumbers = leadDetails.contacts.map(c => `${c.number} (${c.label || 'Primary'})`).join(', ');
+  }
+
   const mailOptions = {
     from: 'leadsnotify.gobindcoach@gmail.com',
     to,
@@ -11,11 +16,15 @@ async function sendLeadNotificationEmail({to, leadId, leadDetails, forwardedBy})
       <p><b>Lead Details:</b></p>
       <ul>
         <li>Lead ID: ${leadId}</li>
-        <li>Client Name: ${leadDetails.clientName}</li>
-        <li>Contact: ${leadDetails.phone}</li>
+        <li>Client Name: ${leadDetails.clientName || '-'}</li>
+        <li>Contact(s): ${contactNumbers}</li>
       </ul>
       <hr>
       <p style="color:#888;font-size:13px;">This is an automated notification. Please do not reply to this email.</p>
+      <p style="margin-top:18px">
+      <b>Check On:</b>
+      <a href="https://leadsmanage.netlify.app/" target="blank" style"color:#3967cb">leadsmanage.netlify.app</a>
+      </p>
     `
   };
 
